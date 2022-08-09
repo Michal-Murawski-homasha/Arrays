8b1ff41aff02170a9635baea3b261d54cb5003c9
Michał Murawski <homasha@gmail.com>
2022-08-09 16:33:41 +0200

    Adds of numbers

--- src/AverageNumbers.java
+++ src/AverageNumbers.java
@@ -3,6 +3,11 @@ import java.util.Arrays;
 public class AverageNumbers {
     public static void main(String[] args) {
         int[] someNumbers = {16, 512, 24, 1024, 8, 4, 64, 32, 128, 2, 256};
-        System.out.print(Arrays.toString(someNumbers));
+        int j = 0;
+        System.out.println("Contets of the array: " + Arrays.toString(someNumbers));
+        for (int i = 0; i < someNumbers.length; i++) {
+                j += someNumbers[i];
+        }
+        System.out.println("Sum = " + j);
     }
 }
