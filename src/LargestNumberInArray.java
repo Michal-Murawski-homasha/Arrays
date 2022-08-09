15fb2be3dade0e3f22f9fa3bf8b7f6d19de9ca83
Michał Murawski <homasha@gmail.com>
2022-08-09 16:01:00 +0200

    Checks and prints largest number

--- src/LargestNumberInArray.java
+++ src/LargestNumberInArray.java
@@ -1,8 +1,12 @@
 public class LargestNumberInArray {
     public static void main(String[] args) {
-        int[] someNumbers = {2, 0, 8};
+        int[] someNumbers = {16, 512, 24, 1024, 8, 4, 64, 32, 128, 2, 256};
+        int j = 0;
         for (int i = 0; i < someNumbers.length; i++) {
-            System.out.println(someNumbers[i]);
+            if (someNumbers[i] > j) {
+                j = someNumbers[i];
+            }
         }
+        System.out.print("The largest number in the array is: " + j);
     }
 }
