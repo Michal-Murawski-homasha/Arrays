de5889af7139a63eebe286337eae0a411d4bf714
Michał Murawski <homasha@gmail.com>
2022-08-09 20:13:44 +0200

    Counts median for an odd index

--- src/Median.java
+++ src/Median.java
@@ -4,7 +4,6 @@ public class Median {
     public static void main(String[] args) {
         int[] someNumbers = {16, 512, 24, 1024, 8, 4, 64, 32, 128, 2, 256};
         System.out.println(Arrays.toString(someNumbers));
-//        int[] sortNumbers = new int[someNumbers.length];
         for (int i = 0; i < someNumbers.length; i++) {
             for (int j = 0; j < someNumbers.length - 1; j++) {
                 if (someNumbers[j] > someNumbers[j + 1]) {
@@ -14,7 +13,14 @@ public class Median {
                 }
             }
         }
-
+            /*int l = (someNumbers.length + 1) / 2;
+            int x = someNumbers[l - 1];
+            int y = someNumbers[l];
+            int sum = (x + y) / 2;*/
+            int l = someNumbers.length / 2;
+            int sum = someNumbers[l];
         System.out.println(Arrays.toString(someNumbers));
+//        System.out.println("x = " + x + ", y = " + y);
+        System.out.println(sum);
     }
 }
