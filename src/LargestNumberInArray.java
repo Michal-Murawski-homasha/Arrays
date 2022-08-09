60d50e3fd79a62d376394e50e397d14abcd36e49
Michał Murawski <homasha@gmail.com>
2022-08-09 15:47:11 +0200

    Initial commit

---
+++ src/LargestNumberInArray.java
@@ -0,0 +1,8 @@
+public class LargestNumberInArray {
+    public static void main(String[] args) {
+        int[] someNumbers = {2, 0, 8};
+        for (int i = 0; i < someNumbers.length; i++) {
+            System.out.println(someNumbers[i]);
+        }
+    }
+}
