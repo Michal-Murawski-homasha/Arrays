9f9b012964d4823f6935b22d1f5499cdf0e5013f
Michał Murawski <homasha@gmail.com>
2022-08-09 18:46:56 +0200

    Adds bubble sorting

--- src/Median.java
+++ src/Median.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+
 public class Median {
     public static void main(String[] args) {
-        C
+        int[] someNumbers = {16, 512, 24, 1024, 8, 4, 64, 32, 128, 2, 256};
+        System.out.println(Arrays.toString(someNumbers));
+//        int[] sortNumbers = new int[someNumbers.length];
+        for (int i = 0; i < someNumbers.length; i++) {
+            for (int j = 0; j < someNumbers.length - 1; j++) {
+                if (someNumbers[j] > someNumbers[j + 1]) {
+                    int tmp = someNumbers[j + 1];
+                    someNumbers[j + 1] = someNumbers[j];
+                    someNumbers[j] = tmp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(someNumbers));
     }
 }
