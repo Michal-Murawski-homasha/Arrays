5c3420cb29b1a7720f0bdd37d768594a58bab60b
Michał Murawski <homasha@gmail.com>
2022-08-09 16:21:49 +0200

    Prints contents of array

--- src/AverageNumbers.java
+++ src/AverageNumbers.java
@@ -1,5 +1,8 @@
+import java.util.Arrays;
+
 public class AverageNumbers {
     public static void main(String[] args) {
-
+        int[] someNumbers = {16, 512, 24, 1024, 8, 4, 64, 32, 128, 2, 256};
+        System.out.print(Arrays.toString(someNumbers));
     }
 }
